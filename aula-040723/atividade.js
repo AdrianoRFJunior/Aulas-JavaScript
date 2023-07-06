6a11d81ae33fbdc964b9fb78f4250fdd7cca40ac
AdrianoRFJunior <adriano.rfjunior@gmail.com>
2023-07-05 22:16:59 -0300

    Estudando-JavaScript

---
+++ aula-040723/atividade.js
@@ -0,0 +1,96 @@
+/*let numero 
+numero = 7;
+
+let resultado 
+resultado = numero % 2 === 0 ? "Par" : "Ímpar";
+console.log(resultado);
+
+let n1 = 19;
+let n2 = 8;
+let n3 = 20;
+let media = (n1 + n2 + n3) / 3;
+console.log(media);
+
+let numero = 15;
+let resultado = numero % 3 === 0 && numero % 5 === 0 ? "Divisível por 3 e por 5" : "Não é divisível por 3 e por 5";
+console.log(resultado);
+
+let base = 4;
+let altura = 5;
+let area = (base * altura) / 2;
+console.log(area);
+
+let celsius = 30;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(fahrenheit);
+
+let a = 1;
+let b = -3;
+let c = 2;
+let delta = b ** 2 - 4 * a * c;
+console.log(delta);
+
+
+let idade = 20;
+let podeDirigir = idade >= 18 ? "Pode dirigir" : "Não pode dirigir";
+console.log(podeDirigir);
+
+//Correçao
+
+// Escreva um programa que verifique se um número é par ou ímpar.
+let num = 98;
+let condicao = (num % 2) == 0; // é par
+let mensagem = condicao ? "Este número é par" : "Este número é ímpar";
+// console.log(mensagem);
+
+// Faça um programa que calcule a média aritmética de três números.
+let n1 = 8;
+let n2 = 3;
+let n3 = 5;
+let media = (n1 + n2 + n3) / 3;
+// console.log("A média é igual a");
+// console.log(media);
+
+// Escreva um programa que determine se um número é divisível por 3 e por 5 ao mesmo tempo.
+let num2 = 20;
+let condicao2 = ((num2 % 3) == 0) && ((num2 % 5) == 0);
+let mensagem2 = condicao2 ? "É divisivel por 3 e 5." : "Não é divisivel por 3 e 5.";
+// console.log(mensagem2);
+
+// Faça um programa que calcule a área de um triângulo com base e altura.
+let base = 5;
+let altura = 8;
+let area = (base * altura) / 2;
+// console.log("A area do triang. com base 4 e altura 8 é");
+// console.log(area);
+
+// Escreva um programa que converta uma temperatura de graus Celsius para Fahrenheit.
+let tempCelsius = 100;
+let tempFahrenh = tempCelsius * 1.8 + 32; // F = C x 1,8 + 32
+// console.log("Celsius");
+// console.log(tempCelsius);
+// console.log("Fahrenheit");
+// console.log(tempFahrenh);
+
+// Faça um programa que calcule o delta das funções de segundo grau.
+// Delta = b^2 - 4 * a * c
+let a = 3; 
+let b = -7;
+let c = 2;
+let delta = b**2 - 4 * a * c;
+// console.log("O valor de delta é");
+// console.log(delta);
+
+// Faça um programa que retorna se uma determinada pessoa pode dirigir, baseado na sua idade (utilize operador ternário).
+let idade = 17;
+let mensagem3 = (idade >= 18) ? "Pode tirar carteira de motorista." : "Não pode tirar carteira de motorista.";
+console.log(mensagem3);
+*/
+
+
+
+
+
+
+
+
